69de6b90b1a988e9016ee913777fc7a4069106ad
Meefr <mohamedelramah737@gmail.com>
2023-07-11 14:08:03 +0300

    new update

--- D03/Tasks/JS/page3.js
+++ D03/Tasks/JS/page3.js
@@ -3,9 +3,9 @@ var telephoneNum = prompt("Enter your Telephone Number: ");
 var mobileNum = prompt("Enter your Mobile Number: "); 
 var email = prompt("Enter your Email Address: ");
 
-document.write("<p> Welcome dear guest <span>"+ username + "</span></p> <br>" );
-document.write("<p> Your telephone number is <span>"+ telephoneNum + "</span></p><br>");
-document.write("<p> Your mobile number is <span>"+ mobileNum + "</span></p><br>");
-document.write("<p> Your email address is <span>"+ email + "</span></p><br>");
+document.write("<p> Welcome dear guest: <span>"+ username + "</span></p> <br>" );
+document.write("<p> Your telephone number is: <span>"+ telephoneNum + "</span></p><br>");
+document.write("<p> Your mobile number is: <span>"+ mobileNum + "</span></p><br>");
+document.write("<p> Your email address is: <span>"+ email + "</span></p><br>");
 
 
